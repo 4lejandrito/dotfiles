ff95134a87ac5c08c1c154df604f9325bad01ecd
Alejandro Tardín <alejandro.tardin@wds.co>
2014-04-09 09:22:26 +0100

    As in wds

--- java/path.zsh
+++ java/path.zsh
@@ -1,1 +1,3 @@
-export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
+export PATH="/home/alex/tools/play-1.2.6-wds-v7":$PATH
+export PATH="/home/alex/tools/play-2.2.1":$PATH
+export PATH="~/tools/eclipse-kepler":$PATH
